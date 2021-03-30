035396dc1d50e3cc7881aae2b58c725a12b7a4a7
Ethan <youngethan4@gmail.com>
2021-03-30 14:32:41 -0500

    Created map with markers.

---
+++ src/Company.ts
@@ -0,0 +1,29 @@
+import faker from 'faker';
+import { Mappable } from './CustomMap';
+
+export class Company implements Mappable {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+      <div>
+        <h2>Company name: ${this.name}</h2>
+        <h3>Catchphrase: ${this.catchPhrase}</h3>
+      </div>
+    `;
+  }
+}
